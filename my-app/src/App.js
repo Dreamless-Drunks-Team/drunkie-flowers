dee9a339c010af77d232b2ac1c779f108541ec8b
GrimLord16 <vplviv16@gmail.com>
2024-05-28 20:16:48 +0300

    added user cabinet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,22 +11,61 @@ const user = {
   lastName: "Turchynyak",
   email: "pavlo.turchynyak@gmail.com",
   status: "LOL,",
-  paymentMethods: ["Lol", "Ce"]
+  paymentMethods: ["Lol", "Ce"],
+  history_of_orders: [
+    {
+      date: "2021-10-10",
+      status: "Delivered",
+      order: [
+        {
+          name: "Bouquet1",
+          price: 1000,
+          quantity: 1,
+          image: 'https://img.freepik.com/free-photo/view-flowers-transparent-glass-with-water-drops_23-2149478590.jpg?t=st=1716916200~exp=1716919800~hmac=5c2c17ed4c1702a0f4b2cd4f4a87cef19bb79057f8d0c3dad7d8269fc1a0655d&w=740'
+        },
+        {
+          name: "Bouquet2",
+          price: 100,
+          quantity: 2,
+          image: 'https://img.freepik.com/free-photo/view-flowers-transparent-glass-with-water-drops_23-2149478590.jpg?t=st=1716916200~exp=1716919800~hmac=5c2c17ed4c1702a0f4b2cd4f4a87cef19bb79057f8d0c3dad7d8269fc1a0655d&w=740'
+        },
+      ],
+    },
+    {
+      date: "2021-10-11",
+      status: "Delivered",
+      order: [
+        {
+          name: "Bouquet3",
+          price: 200,
+          quantity: 1,
+          image: 'https://img.freepik.com/free-photo/view-flowers-transparent-glass-with-water-drops_23-2149478590.jpg?t=st=1716916200~exp=1716919800~hmac=5c2c17ed4c1702a0f4b2cd4f4a87cef19bb79057f8d0c3dad7d8269fc1a0655d&w=740'
+        },
+        {
+          name: "Bouquet4",
+          price: 300,
+          quantity: 1,
+          image: 'https://img.freepik.com/free-photo/view-flowers-transparent-glass-with-water-drops_23-2149478590.jpg?t=st=1716916200~exp=1716919800~hmac=5c2c17ed4c1702a0f4b2cd4f4a87cef19bb79057f8d0c3dad7d8269fc1a0655d&w=740'
+        },
+      ],
+    },
+  ],
 }
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegistrationForm />} />
-        <Route path="/profile" element={<UserProfile user={user}/>} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+function App() { 
+  return ( 
+    <BrowserRouter> 
+      <Header /> 
+      <Routes> 
+        <Route path="/" element={<Home bouquetList={bouquetList.slice(0, 5)}/>} /> 
+        <Route path="/login" element={<LoginForm />} /> 
+        <Route path="/register" element={<RegistrationForm />} /> 
+        <Route path="/profile" element={<UserProfile user={user} />} /> 
+        <Route path="/catalog" element={<Catalog bouquetList={bouquetList}/>} /> 
+      </Routes> 
+      <Footer /> 
+    </BrowserRouter> 
+  ); 
 }
 
 export default App;
--- my-app/src/page/UserAccount/UserProfile.jsx
+++ my-app/src/page/UserAccount/UserProfile.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
-import './UserProfile.scss'
+import React, { useState } from 'react';
+import './UserProfile.scss'; // Regular SCSS import
+
 const UserProfile = ({ user }) => {
+  const [showDetails, setShowDetails] = useState(user.history_of_orders.map(() => false));
+
+const toggleDetails = index => {
+  const updatedShowDetails = [...showDetails];
+  updatedShowDetails[index] = !updatedShowDetails[index];
+  setShowDetails(updatedShowDetails);
+};
+
   return (
     <div className="user-profile">
       <h2>Інформація про користувача</h2>
@@ -13,7 +22,33 @@ const UserProfile = ({ user }) => {
       </div>
       <h2>Історія замовлень</h2>
       <div>
-        {/* Цей блок можна заповнити замовленнями користувача */}
+        {user.history_of_orders.map((orderHistory, index) => (
+          <div key={index} className="order-history">
+            <p><strong>Дата оформлення замовлення:</strong> {orderHistory.date}</p>
+            <p><strong>Статус:</strong> {orderHistory.status}</p>
+            <button onClick={() => toggleDetails(index)}>
+              {showDetails[index] ? 'Приховати деталі' : 'Показати деталі'}
+            </button>
+            {showDetails[index] && (
+              <ul>
+                {orderHistory.order.map((orderItem, idx) => (
+                  <li key={idx} className="order-item">
+                    <div className="order-info">
+                      <p><strong>Назва:</strong> {orderItem.name}</p>
+                      <p><strong>Ціна:</strong> {orderItem.price} грн</p>
+                      <p><strong>Кількість:</strong> {orderItem.quantity}</p>
+                    </div>
+                    {orderItem.image && (
+                      <div className="order-image">
+                        <img src={orderItem.image} alt={`bouquet-${index}`} />
+                      </div>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
