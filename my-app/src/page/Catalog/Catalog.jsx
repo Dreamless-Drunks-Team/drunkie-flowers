69f2c1d469c3fa0251d86c9214bc300c8293c90e
Turchynyak <pavlo.turchynyak@infineon.com>
2024-05-28 20:27:02 +0300

    Updated style and filter for Catalog Page

--- my-app/src/page/Catalog/Catalog.jsx
+++ my-app/src/page/Catalog/Catalog.jsx
@@ -1,5 +1,5 @@
 import BouquetItem from '../../components/BouquetItem/BouquetItem';
-import './Catalog.scss'
+import './Catalog.scss';
 
 const Catalog = ({ bouquetList }) => {
   return (
@@ -9,29 +9,32 @@ const Catalog = ({ bouquetList }) => {
         <div className="filter-section">
           <h4>Сезон</h4>
           <ul>
-            <li>Зимовий</li>
-            <li>Осінній</li>
-            <li>Літній</li>
-            <li>Весняний</li>
+            <li><input type="checkbox" id="winter" /><label for="winter">Зимовий</label></li>
+            <li><input type="checkbox" id="spring" /><label for="spring">Весняний</label></li>
+            <li><input type="checkbox" id="summer" /><label for="summer">Літній</label></li>
+            <li><input type="checkbox" id="autumn" /><label for="autumn">Осінній</label></li>
           </ul>
         </div>
         <div className="filter-section">
           <h4>Події</h4>
           <ul>
-            <li>Весілля</li>
-            <li>Похорони</li>
-            <li>Дні народження</li>
-            <li>Інші свята</li>
+            <li><input type="checkbox" id="wedding" /><label for="wedding">Весілля</label></li>
+            <li><input type="checkbox" id="funeral" /><label for="funeral">Похорони</label></li>
+            <li><input type="checkbox" id="birthday" /><label for="birthday">Дні народження</label></li>
+            <li><input type="checkbox" id="other" /><label for="other">Інші свята</label></li>
+          </ul>
+        </div>
+        <div className="filter-section">
+          <h4>Букети зі знижкою</h4>
+          <ul>
+            <li><input type="checkbox" id="discount" /><label for="discount">Зі знижкою</label></li>
+            <li><input type="checkbox" id="noDiscount" /><label for="noDiscount">Без знижки</label></li>
           </ul>
         </div>
         <div className="filter-section">
           <h4>Діапазон ціни</h4>
-          <input type="text" placeholder="Від" />
-          <input type="text" placeholder="До" />
-        </div>
-        <div className="filter-section">
-          <h4>Букети зі знижкою</h4>
-          <input type="checkbox" />
+          <input type="number" placeholder="Від" />
+          <input type="number" placeholder="До" />
         </div>
       </div>
       <div className="bouquet-list">
