98fe9ae01ee1c69baa85f81b98f3e874425e5491
MykolaKovalyk <mykola.kovalyk.public@gmail.com>
2024-05-28 23:24:59 +0300

    Add file server

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from orm.db import db
@@ -10,10 +11,12 @@ CORS(app)
 from api.orders import orders
 from api.bouquets import bouquets
 from api.user_profile import user_profile
+from api.fileserver import fileserver
 
-app.register_blueprint(orders, url_prefix='/orders')
-app.register_blueprint(bouquets, url_prefix='/bouquets')
-app.register_blueprint(user_profile, url_prefix='/user')
+app.register_blueprint(orders, url_prefix="/orders")
+app.register_blueprint(bouquets, url_prefix="/bouquets")
+app.register_blueprint(user_profile, url_prefix="/user")
+app.register_blueprint(fileserver, url_prefix="/files")
 
 from orm.bouquet import *
 from orm.event import *
@@ -25,8 +28,10 @@ from orm.user import *
 from orm.card import *
 from orm.flower import *
 
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-app.config['SQLALCHEMY_ECHO'] = True
+app.config["UPLOAD_FOLDER"] = app.static_folder
+app.config["SQLALCHEMY_ECHO"] = True
 
 db.init_app(app)
 app.app_context().push()
@@ -35,5 +40,5 @@ db.drop_all()
 db.create_all()
 populate_db()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
